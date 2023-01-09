38a572e98adbbf34fb03fc14326c7d0c6c1b8589
nondefault <spam@nondefault.net>
2023-01-09 13:17:28 -0800

    lat?

--- lzscheme.py
+++ lzscheme.py
@@ -1,20 +1,49 @@
 from sys import stdin
+import traceback
 
 class UnmatchedParenthesesError(RuntimeError):
   def __init__(self, message, *, depth):
     self.message = message
     self.depth = depth
 
+class Context:
+  def __init__(self, *, names={}):
+    self.names = names
+
+  def copy(self):
+    return Context(names=self.names.copy())
+
+  def define(self, name, value):
+    self.names[name] = value
+    return self
+
+  def resolve(self, name):
+    if isinstance(name, str) and name in self.names:
+      return self.names[name]
+    return name
+
 def is_list(l):
   return isinstance(l, list)
 
+def list_fn(context, l):
+  context, l = seval(context, l)
+  return context, is_list(l)
+
 def is_atom(src):
   return not is_list(src)
+
+def atom_fn(context, sexpr):
+  context, sexpr = seval(context, sexpr)
+  return context, is_atom(sexpr)
 
 def is_null(l):
   if not is_list(l):
     raise Exception(f'Argument {l} is not a list')
   return len(l) == 0
+
+def null_fn(context, l):
+  context, l = seval(context, l)
+  return context, is_null(l)
 
 def is_eq(a, b):
   if not is_atom(a):  
@@ -23,32 +52,69 @@ def is_eq(a, b):
     raise Exception(f'Argument {b} is not an atom')
   return a == b
 
-def car(l):
+def eq_fn(context, a, b):
+  context, a = seval(context, a)
+  context, b = seval(context, b)
+  return context, is_eq(a, b)
+
+def car(context, l):
+  context, l = seval(context, l)
   if not isinstance(l, list):
     raise Exception(f'Argument {l} is not a list')
   if not len(l) > 0:
     raise Exception(f'Argument is null list')
-  return l[0]
+  return context, l[0]
 
-def cdr(l):
+def cdr(context, l):
+  context, l = seval(context, l)
   if not isinstance(l, list):
     raise Exception(f'Argument {l} is not a list')
   if not len(l) > 0:
     raise Exception(f'Argument is null list')
-  return l[1:]
+  return context, l[1:]
 
-def cons(a, l):
+def cons(context, a, l):
+  context, a = seval(context, a)
+  context, l = seval(context, l)
   if not isinstance(l, list):
     raise Exception(f'Argument {l} is not a list')
-  return [a, *l]
+  return context, [a, *l]
 
-func_table = {
+def cond(context, *clauses):
+  for [test, then] in clauses:
+    context, test_result = seval(context, test)
+    if test_result == True or test_result == 'else':
+      context, then_result = seval(context, then)
+      return context, then_result
+  return context, None
+
+def define_fn(context, name, sexpr):
+  return context.copy().define(name, sexpr), None
+
+def lambda_fn(context, params, sexpr):
+  if not is_list(params):
+    raise Exception(f'Parameters must be a list, got: {params}')
+
+  def f(f_context, *args):
+    new_f_context = f_context.copy()
+    for param, arg in zip(params, args):
+      new_f_context, arg = seval(new_f_context, arg)
+      new_f_context.define(param, arg)
+    new_f_context, result = seval(new_f_context, sexpr)
+    return new_f_context, result
+
+  return context, f
+
+builtin_func_table = {
   'car': car,
   'cdr': cdr,
   'cons': cons,
-  'atom?': is_atom,
-  'null?': is_null,
-  'eq?': is_eq,
+  'cond': cond,
+  'define': define_fn,
+  'lambda': lambda_fn,
+  'atom?': atom_fn,
+  'null?': null_fn,
+  'eq?': eq_fn,
 }
 
 def parse(src):
@@ -84,43 +150,63 @@ def parse(src):
 
   return sexprs
 
-def stringify(sexprs):
-  return ' '.join(f'({stringify(x)})' if not is_atom(x) else str(x) for x in sexprs)
+def stringify(context, sexprs):
+  if not is_list(sexprs):
+    return str(sexprs)
+  return ' '.join(f'({stringify(context, x)})' if not is_atom(x) else str(x) for x in sexprs)
 
-def seval(sexprs):
-  return [seval_list(sexpr) if is_list(sexpr) else sexpr for sexpr in sexprs]
-
-def seval_list(sexprs):
+def seval_list(context, sexprs):
+  print('seval_list', stringify(context, sexprs))
   if len(sexprs) > 0:
-    func_name = sexprs[0]
-    if is_atom(func_name) and func_name in func_table:
+    context, func = seval(context, sexprs[0])
+    print('  func', func)
+    if callable(func):
       args = sexprs[1:]
-      print('  calling', func_name, args)
-      res = func_table[func_name](*args)
-      print('  result', res)
-      return res
-  return sexprs
+      print('  calling', sexprs[0], args)
+      context, result = func(context, *args)
+      print('  result', result)
+      return context, result
+  return context, sexprs
+
+def seval(context, sexpr):
+  print('seval', stringify(context, sexpr))
+  sexpr = context.resolve(sexpr)
+  print('  resolved:', stringify(context, sexpr))
+  if is_list(sexpr):
+    return seval_list(context, sexpr)
+  return context, sexpr
+
+def seval_multiple(context, sexprs):
+  results = []
+  for sexpr in sexprs:
+    context, result = seval(context, sexpr)
+    results.append(result)
+  return context, results
 
 def main():
+  context = Context(names=builtin_func_table.copy())
   input_buffer = ''
   input_depth = 0
+
   while True:
     prompt = '> ' if input_depth == 0 else '  ' * input_depth + '  '
     print(prompt, end='', flush=True)
     input_buffer += stdin.readline()
     try:
-      parsed = parse(input_buffer)
-      print(stringify(seval(parsed)))
-      print()
-      input_buffer = ''
-      input_depth = 0
-    except UnmatchedParenthesesError as e:
-      if e.depth == 0:
-        print(e)
-      else:
-        input_depth = e.depth
+      try:
+        parsed = parse(input_buffer)
+        context, results = seval_multiple(context, parsed)
+        print(stringify(context, results))
+        print()
+        input_buffer = ''
+        input_depth = 0
+      except UnmatchedParenthesesError as e:
+        if e.depth > 0:
+          input_depth = e.depth
+        else:
+          raise e
     except Exception as e:
-      print(e)
+      traceback.print_exc()
       print()
       input_buffer = ''
       input_depth = 0
