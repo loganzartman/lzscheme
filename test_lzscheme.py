2d5434a30f865405d2823104542c8c668a3a0012
nondefault <spam@nondefault.net>
2023-02-01 01:25:15 -0800

    fix tests

--- test_lzscheme.py
+++ test_lzscheme.py
@@ -2,8 +2,10 @@ from typing import Any, Optional
 from lzscheme import parse as parse_internal, run, stringify_callstack
 from lzscheme import Pair, Sexpr, Env, EvalError, NativeFunction
 
-# temporary bridge for converting tests
-def pythonify(s: Sexpr) -> Any:
+# make assertions easier
+def pythonify(s: Optional[Sexpr]) -> Any:
+  if s is None:
+    return s
   if isinstance(s, Pair):
     return [pythonify(el) for el in s]
   if isinstance(s, NativeFunction):
@@ -19,84 +21,87 @@ def run_results(src: str, env: Optional[Env]=None):
   return pythonify(results)
 
 def test_parse_atoms():
-  assert parse('atom') == ['atom']
-  assert parse('an atom') == ['an', 'atom']
-  assert parse('so  many   atoms') == ['so', 'many', 'atoms']
+  assert parse('atom') == 'atom'
+  assert parse('another_one') == 'another_one'
+  assert parse('+') == '+'
 
 def test_parse_lists():
-  assert parse('( )') == [[]]
-  assert parse('( atom )') == [['atom']]
-  assert parse('(atom)') == [['atom']]
-  assert parse('(atoms are everywhere)') == [['atoms', 'are', 'everywhere']]
-  assert parse('(lists) (hold) (many things)') == [['lists'], ['hold'], ['many', 'things']]
+  assert parse('( )') == []
+  assert parse('( atom )') == ['atom']
+  assert parse('(atom)') == ['atom']
+  assert parse('(atoms are everywhere)') == ['atoms', 'are', 'everywhere']
 
 def test_parse_nested():
-  assert parse('(())') == [[[]]]
-  assert parse('(atoms (and lists))') == [['atoms', ['and', 'lists']]]
-  assert parse('nested (lists are (very (fun)))') == ['nested', ['lists', 'are', ['very', ['fun']]]]
+  assert parse('(())') == [[]]
+  assert parse('(atoms (and lists))') == ['atoms', ['and', 'lists']]
+  assert parse('(nested (lists are (very) fun))') == ['nested', ['lists', 'are', ['very'], 'fun']]
 
 def test_parse_string():
-  assert parse('"string"') == ['string']
-  assert parse('a "b" c') == ['a', 'b', 'c']
-  assert parse('(a "b" c)') == [['a', 'b', 'c']]
-  assert parse('("apples" ("bananas" "and" "citrus"))') == [['apples', ['bananas', 'and', 'citrus']]]
+  assert parse('"string"') == 'string'
+  assert parse('(a "b" c)') == ['a', 'b', 'c']
+  assert parse('("apples" ("bananas" "and" "citrus"))') == ['apples', ['bananas', 'and', 'citrus']]
 
 def test_parse_string_empty():
-  assert parse('""') == ['']
+  assert parse('""') == ''
 
 def test_parse_simple_escapes():
-  assert parse(r'"string\"s"') == ['string"s']
-  assert parse(r'"tab\ttab\ttab"') == ['tab\ttab\ttab']
+  assert parse(r'"string\"s"') == 'string"s'
+  assert parse(r'"tab\ttab\ttab"') == 'tab\ttab\ttab'
 
 def test_parse_hex_escapes():
-  assert parse(r'"unicod\x65;"') == ['unicode']
-  assert parse(r'"\x443;\x43a;\x440;\x430;\x457;\x43d;\x0456;"') == ['україні']
+  assert parse(r'"unicod\x65;"') == 'unicode'
+  assert parse(r'"\x443;\x43a;\x440;\x430;\x457;\x43d;\x0456;"') == 'україні'
 
 def test_parse_numbers():
-  assert parse('14') == [14]
-  assert parse('-3') == [-3]
-  assert parse('3.14159') == [3.14159]
-  assert parse('a 14') == ['a', 14]
+  assert parse('14') == 14
+  assert parse('-3') == -3
+  assert parse('3.14159') == 3.14159
+  assert parse('(a 14)') == ['a', 14]
 
 def test_eval_atom():
-  assert run_results('atom') == ['atom']
+  assert run_results('atom') == 'atom'
 
 def test_eval_list():
-  assert run_results('(atom)') == [['atom']]
+  assert run_results('(atom)') == ['atom']
 
 def test_car():
-  assert run_results('(car (a b c))') == ['a']
+  assert run_results('(car (a b c))') == 'a'
 
 def test_cdr():
-  assert run_results('(cdr (a b c))') == [['b', 'c']]
+  assert run_results('(cdr (a b c))') == ['b', 'c']
 
 def test_cons():
-  assert run_results('(cons a (b c))') == [['a', 'b', 'c']]
+  assert run_results('(cons a (b c))') == ['a', 'b', 'c']
 
 def test_atom_fn():
-  assert run_results('(atom? atom)') == [True]
-  assert run_results('(atom? (atom))') == [False]
+  assert run_results('(atom? atom)') == True
+  assert run_results('(atom? (atom))') == False
 
 def test_null_fn():
-  assert run_results('(null? ())') == [True]
-  assert run_results('(null? (atom))') == [False]
+  assert run_results('(null? ())') == True
+  assert run_results('(null? (atom))') == False
 
 def test_eq_fn():
-  assert run_results('(eq? atom atom)') == [True]
-  assert run_results('(eq? atom banana)') == [False]
+  assert run_results('(eq? atom atom)') == True
+  assert run_results('(eq? atom banana)') == False
 
 def test_argument_scoping():
   result = run_results('''
     (define test
       (lambda (a) a))
     (test 1234)
+  ''')
+
+  assert str(result) == '1234'
+
+  result = run_results('''
+    (define test
+      (lambda (a) a))
     a
   ''')
 
-  assert str(result[0]) == '1234'
-
-  assert str(result[1]) != '1234'
-  assert result[1] == 'a'
+  assert str(result) != '1234'
+  assert result == 'a'
 
 def test_lat():
   lat = '''
@@ -111,12 +116,12 @@ def test_lat():
   assert run_results(f'''
     {lat}
     (lat? (bacon and eggs))
-  ''') == [True]
+  ''') == True
 
   assert run_results(f'''
     {lat}
     (lat? (bacon (and eggs)))
-  ''') == [False]
+  ''') == False
 
 def test_member():
   member = '''
@@ -130,12 +135,12 @@ def test_member():
   assert run_results(f'''
     {member}
     (member? meat (mashed potatoes and meat gravy))
-  ''') == [True]
+  ''') == True
 
   assert run_results(f'''
     {member}
     (member? meat (mashed potatoes and meat gravy))
-  ''') == [True]
+  ''') == True
 
 def test_rember():
   rember = '''
@@ -150,17 +155,17 @@ def test_rember():
   assert run_results(f'''
     {rember}
     (rember meat (mashed potatoes and meat gravy))
-  ''') == [['mashed', 'potatoes', 'and', 'gravy']]
+  ''') == ['mashed', 'potatoes', 'and', 'gravy']
 
   assert run_results(f'''
     {rember}
     (rember mint (lamb chops and mint flavored mint jelly))
-  ''') == [['lamb', 'chops', 'and', 'flavored', 'mint', 'jelly']]
+  ''') == ['lamb', 'chops', 'and', 'flavored', 'mint', 'jelly']
 
   assert run_results(f'''
     {rember}
     (rember toast (bacon lettuce and tomato))
-  ''') == [['bacon', 'lettuce', 'and', 'tomato']]
+  ''') == ['bacon', 'lettuce', 'and', 'tomato']
 
 def test_firsts():
   firsts = '''
@@ -178,7 +183,7 @@ def test_firsts():
       (plum pear cherry)
       (grape raisin pea)
       (bean carrot eggplant)))
-  ''') == [['apple', 'plum', 'grape', 'bean']]
+  ''') == ['apple', 'plum', 'grape', 'bean']
   
   assert run_results(f'''
     {firsts}
@@ -187,21 +192,21 @@ def test_firsts():
       (plum pear cherry)
       (grape raisin pea)
       (bean carrot eggplant)))
-  ''') == [['apple', 'plum', 'grape', 'bean']]
+  ''') == ['apple', 'plum', 'grape', 'bean']
 
 def test_load():
-  assert run_results('(load std.scm) (member? a (a b c))')[1] == True
+  assert run_results('(load std.scm) (member? a (a b c))') == True
 
 def test_math():
-  assert run_results('(+ 1 2)') == [3]
-  assert run_results('(- 3 4)') == [-1]
-  assert run_results('(* 5 6)') == [30]
-  assert run_results('(/ 6 2)') == [3]
+  assert run_results('(+ 1 2)') == 3
+  assert run_results('(- 3 4)') == -1
+  assert run_results('(* 5 6)') == 30
+  assert run_results('(/ 6 2)') == 3
 
-  assert run_results('(add1 67)') == [68]
-  assert run_results('(sub1 5)') == [4]
-  assert run_results('(zero? 0)') == [True]
-  assert run_results('(zero? 1492)') == [False]
+  assert run_results('(add1 67)') == 68
+  assert run_results('(sub1 5)') == 4
+  assert run_results('(zero? 0)') == True
+  assert run_results('(zero? 1492)') == False
 
 def test_traceback():
   try:
@@ -210,10 +215,9 @@ def test_traceback():
   except EvalError as e:
     lines = stringify_callstack(e.callstack).split('\n')
     assert '0' in lines[0]
-    assert '((cons a (cons b (abort))))' in lines[0]
-    assert '(cons a (cons b (abort)))' in lines[1]
-    assert '(cons b (abort))' in lines[2]
-    assert '(abort)' in lines[3]
-    assert '3' in lines[3]
+    assert '(cons a (cons b (abort)))' in lines[0]
+    assert '(cons b (abort))' in lines[1]
+    assert '(abort)' in lines[2]
+    assert '2' in lines[2]
   except:
     assert False
